b94f1f3726ec78df96ad2dc10c4b294aa654c489
RiccardoCecco <ceccoriccardo1997@gmail.com>
2023-10-19 11:48:43 +0200

    change trunk

--- scripts/nord/app.sh
+++ scripts/nord/app.sh
@@ -6,7 +6,7 @@ NUM_NODES=$5
 EXEC_TIME=$6
 QOS=$7
 INSTALL_DIR=$8
-module load compss/TrunkRC
+module load compss/3.2
 # shellcheck disable=SC2164
 cd $EXECUTION_FOLDER
 enqueue_compss -g -t -d --worker_working_dir=$PWD --scheduler=es.bsc.compss.scheduler.orderstrict.fifo.FifoTS --job_execution_dir=$EXECUTION_FOLDER --qos=$QOS --exec_time=$EXEC_TIME --pythonpath=$PYTHONPATH --num_nodes=$NUM_NODES --worker_in_master_cpus=16 $INSTALL_DIR/BACKEND/api.py $WORKFLOW_FOLDER/$NAMEFILE $EXECUTION_FOLDER
