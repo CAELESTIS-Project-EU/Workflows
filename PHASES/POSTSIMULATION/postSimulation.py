da01698253f75e170eabc1e42fbe9a6fe27c8898
RiccardoCecco <ceccoriccardo1997@gmail.com>
2023-11-21 17:37:15 +0100

    upload file ftp server

--- PHASES/POSTSIMULATION/postSimulation.py
+++ PHASES/POSTSIMULATION/postSimulation.py
@@ -10,8 +10,22 @@ def collect(type, wdir, nameSim, out):
 
 @task(y_param=COLLECTION_IN)
 def write_file(type, output_folder, y_param, out, **kwargs):
-    file=str(kwargs.get("outputs").get("alya-output").get("path"))
-    y_file=output_folder+"/"+file
-    module = importlib.import_module('PHASES.POSTSIMULATION.' + type)
-    getattr(module, 'write_yFile')(y_file, y_param)
+    outputs = kwargs.get("outputs")
+    alya_output_list = outputs.get("alya-output")
+    # Initialize file_path to None
+    file_path = None
+    # Iterate over each dictionary in the list
+    for item in alya_output_list:
+        if "path" in item:
+            file_path = item["path"]
+            break  # Stop the loop once the path is found
+    # Check if file_path was found
+    if file_path is not None:
+        # You can now use file_path for further processing
+        file = str(file_path)
+        y_file = output_folder + "/" + file
+        module = importlib.import_module('PHASES.POSTSIMULATION.' + type)
+        getattr(module, 'write_yFile')(y_file, y_param)
+    return
+
 
