f9528d4f6358deaa995e3bdc0694b23716378399
RiccardoCecco <ceccoriccardo1997@gmail.com>
2023-11-27 16:21:01 +0100

    up

--- PHASES/MODEL_TRAINING/trainingModel.py
+++ PHASES/MODEL_TRAINING/trainingModel.py
@@ -4,6 +4,7 @@ from pycompss.api.parameter import *
 
 
 #@task(returns=1)
-def training(training_type, **kwargs):
+def training(training, **kwargs):
+    training_type=training.get("type")
     module = importlib.import_module('PHASES.MODEL_TRAINING.' + training_type)
     return getattr(module, 'training')(**kwargs)
