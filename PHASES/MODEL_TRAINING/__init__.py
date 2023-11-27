20861af931a949604d4de78d6384e5b3c12ec045
RiccardoCecco <ceccoriccardo1997@gmail.com>
2023-11-27 16:17:26 +0100

    up

--- PHASES/MODEL_TRAINING/__init__.py
+++
@@ -1,5 +0,0 @@
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, RationalQuadratic as RQ, ExpSineSquared as Exp, DotProduct, WhiteKernel
-from sklearn.gaussian_process.kernels import Matern
-
-def model_creation():
