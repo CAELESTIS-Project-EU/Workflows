d9a4419782f1fb3b7d40621d2cccef514a824c55
RiccardoCecco <ceccoriccardo1997@gmail.com>
2023-11-28 15:12:17 +0100

    uP

--- PHASES/MODEL_TRAINING/GaussianProcess.py
+++ PHASES/MODEL_TRAINING/GaussianProcess.py
@@ -4,36 +4,34 @@ from dislib.model_selection import GridSearchCV
 import dislib as ds
 import importlib
 import pandas as pd
+import numpy as np
 
 
 def training(training, **kwargs):
-    # outputs = kwargs.get("outputs")
-    # input = kwargs.get("input")
-    # x = float(input.get("x"))
-    # y = float(input.get("y"))
+    #outputs = kwargs.get("outputs")
+    #input = kwargs.get("input")
+    #x = float(input.get("x"))
+    #y = float(input.get("y"))
     x, y = make_regression(n_samples=5, n_features=2, noise=1, random_state=42)
     gen_model(x, y, training)
     return
-
 
 def gen_parameters(kernel_type, **kwargs):
     module = importlib.import_module('PHASES.MODEL_TRAINING.PARAMETERS.' + kernel_type)
     return getattr(module, 'gen_param')(**kwargs)
 
-
 def gen_model(x, y, training):
-    kernel_type = training['kernel']['name']
-    parameters = training['kernel']['parameters']
+    kernel_type= training['kernel']['name']
+    parameters= training['kernel']['parameters']
     gpr = GaussianProcessRegressor()
-    # gpr.kernel = gen_parameters(kernel_type, parameters=parameters)
-    # gpr.random_state = 0
-    params = {"kernel": [gen_parameters(kernel_type, parameters=parameters)], "random_state": [0]}
+    #gpr.kernel = gen_parameters(kernel_type, parameters=parameters)
+    #gpr.random_state = 0
+    params= { "kernel" : [gen_parameters(kernel_type, parameters=parameters)], "random_state": [0]}
     # use grid search with your training data (it might take a while, be patient)
     searcher = GridSearchCV(gpr, params)
-    x_block_size = (2, 2)  # Adjust based on your data and environment
-    y_block_size = (2,)
-    x_ds = ds.array(x, block_size=x_block_size)
-    y_ds = ds.array(y, block_size=y_block_size)
-    searcher.fit(x_ds, y_ds)
+    x = ds.array(x, block_size=x.shape)
+    y = y[:, np.newaxis]
+    y = ds.array(y, block_size=y.shape)
+    searcher.fit(x, y)
     print("RESULT")
     print(pd.DataFrame(searcher.cv_results_)[["params", "mean_test_score"]])
