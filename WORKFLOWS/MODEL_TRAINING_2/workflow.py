7eeb413806edee8e50e999d1c2cc6b4d37cc8760
RiccardoCecco <ceccoriccardo1997@gmail.com>
2023-11-29 17:09:38 +0100

    new

--- WORKFLOWS/MODEL_TRAINING_2/workflow.py
+++ WORKFLOWS/MODEL_TRAINING_2/workflow.py
@@ -13,8 +13,7 @@ def workflow(path, execution_folder, data_folder):
 def workflow_execution(training):
     x = np.array([[1.27979699e+00, 3.47209053e+01, 1.97452797e+03],
              [1.29823335e+00, 3.40782408e+01, 2.10593509e+03]])
-    y = np.array([[20118.888],
-             [20424.085]])
+    y = [20118.888, 20424.085]
 
     results_folder="/home/bsc19/bsc19518/result_p/"
     res = train.training(x, y, training)
