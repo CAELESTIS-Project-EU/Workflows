86b85fb0dd4759d9202d7a67494a8a8b70030a2d
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-01-02 16:24:43 +0100

    done

--- PHASES/SIMULATIONS/alya.py
+++ PHASES/SIMULATIONS/alya.py
@@ -1,7 +1,7 @@
 from pycompss.api.mpi import mpi
 from pycompss.api.task import task
 from pycompss.api.parameter import *
-@mpi(runner="mpirun", binary="$ALYA_BIN", args="{{name}} ", processes="{{cpus}}", processes_per_node="$ALYA_PPN", working_dir="{{wdir}}")
+@mpi(runner="mpirun", binary="$ALYA_BIN", args="{{name}} ", processes="$ALYA_PROCS", processes_per_node="$ALYA_PPN", working_dir="{{wdir}}")
 @task(returns=1, time_out=3600)
 def simulation(wdir, name, **kwargs):
         cpus=64
