773ec87a610397fe92ac8283b5e591c1614f7d4c
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-01-02 16:22:09 +0100

    done

--- PHASES/SIMULATIONS/alya.py
+++ PHASES/SIMULATIONS/alya.py
@@ -4,9 +4,8 @@ from pycompss.api.parameter import *
 @mpi(runner="mpirun", binary="$ALYA_BIN", args="{{name}} ", processes="{{cpus}}", processes_per_node="$ALYA_PPN", working_dir="{{wdir}}")
 @task(returns=1, time_out=3600)
 def simulation(wdir, name, **kwargs):
+        cpus=64
         if kwargs.get("sim_params"):
                 sim_params = kwargs.get("sim_params")
                 cpus= int(sim_params.get("cpus", 64))
-        else:
-                cpus=64
         return
