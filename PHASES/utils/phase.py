665348bd4474fa499fca9d1383cd11655c8562cd
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-01-10 17:56:29 +0100

    ciaone

--- PHASES/utils/phase.py
+++ PHASES/utils/phase.py
@@ -9,6 +9,8 @@ def run(phase_info, **kwargs):
     phase_function, phase_args = phase_info
     module_call, function_call = split_string_at_last_dot(phase_function)
     module = importlib.import_module(module_call)
+    print("HEREE")
+    print("prepare_args", phase_args)
     return getattr(module, function_call)(phase_args, **kwargs)
 
 
