26ede4b124a27f1881a64a17869e96c619a606c6
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-01-10 16:49:55 +0100

    try

--- PHASES/utils/phase.py
+++ PHASES/utils/phase.py
@@ -9,11 +9,9 @@ def run(phase_function, phase_args, **kwargs):
 
 def split_string_at_last_dot(input_string):
     last_dot_index = input_string.rfind('.')
-
     if last_dot_index != -1:
         first_part = input_string[:last_dot_index]
         last_part = input_string[last_dot_index + 1:]
         return first_part, last_part
     else:
-        # If there is no dot in the input string
-        return None, None
+        raise ValueError
