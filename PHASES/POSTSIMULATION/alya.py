b2237038b0256c447a526f83af87879087873a6d
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-01-12 17:03:21 +0100

    update

--- PHASES/POSTSIMULATION/alya.py
+++ PHASES/POSTSIMULATION/alya.py
@@ -23,8 +23,8 @@ def collect_results(post_process_args, **kwargs):
 def write_results(write_file_args, **kwargs):
     alya_output = get_value(write_file_args, "alya_output")
     if alya_output is not None:
-        result_folder = write_file_args.get("result_folder")
-        y_elements = write_file_args.get("y")
+        result_folder =get_value(write_file_args, "result_folder")
+        y_elements = get_value(write_file_args, "y")
         y_file = os.path.join(result_folder, alya_output)
         with open(y_file, 'w') as f3:
             f3.write("Y COLLECT: \n")
