6c517a2d54c6f3972ff4ad0147c5683b25de85a2
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-01-15 15:46:33 +0100

    try

--- PHASES/BEFORESIMULATION/alya.py
+++ PHASES/BEFORESIMULATION/alya.py
@@ -126,7 +126,7 @@ def prepare_dom(prepare_args, **kwargs):
     return
 
 def get_value(element, param):
-    if element[param]:
+    if param in element:
         return element[param]
     else:
         raise ValueError(f"The key '{param}' was not found in the dictionary.")
