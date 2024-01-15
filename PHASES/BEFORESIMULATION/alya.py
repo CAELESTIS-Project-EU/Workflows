5f2b5ecad5e7fe52055de74692a2196b40ac0cbe
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-01-15 14:58:49 +0100

    new type of passing parameters

--- PHASES/BEFORESIMULATION/alya.py
+++ PHASES/BEFORESIMULATION/alya.py
@@ -8,6 +8,8 @@ from pycompss.api.parameter import *
 
 def prepare_data(**kwargs):
     prepare_args = kwargs.get("args")
+    print("prepare_args")
+    print(prepare_args)
     variables = vars_func(prepare_args)
     out1 = prepare_sld(prepare_args, variables)
     bool_temlate_fie=check_template_exist(prepare_args,"template_fie")
