87e49f4c5ffa08e861d0f0b10300054c998f39f8
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-02-14 16:21:48 +0100

    update

--- WORKFLOWS/PERMEABILITY/workflow.py
+++ WORKFLOWS/PERMEABILITY/workflow.py
@@ -10,6 +10,10 @@ def execution(yaml_file, execution_folder, data_folder, parameters):
 
 
 def workflow_execution(phases, yaml_file, execution_folder, data_folder, parameters):
+    print("PARAMETERS")
+    print(parameters)
+    print("yaml_file")
+    print(yaml_file)
     sample_set = phase.run(args_values.get_values(phases.get("sampler"), yaml_file, data_folder, locals()))
     sample_set = compss_wait_on(sample_set)
     original_name_sim = parameters.get("original_name_sim")
