0960db2f791f1a3f12d263dbbe8611231dcfadcf
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-02-19 16:42:00 +0100

    Update

--- PHASES/SIMULATIONS/permeability_alya.py
+++ PHASES/SIMULATIONS/permeability_alya.py
@@ -5,9 +5,9 @@ from pycompss.api.task import task
 from pycompss.api.parameter import *
 
 
-@mpi(runner="mpirun", binary="$ALYA_BIN", args="{{name_sim}}", processes="$ALYA_PROCS", processes_per_node="$ALYA_PPN", working_dir="{{simulation_wdir}}")
+@mpi(runner="mpirun", binary="$ALYA_BIN", args="{{case_name}}", processes="$ALYA_PROCS", processes_per_node="$ALYA_PPN", working_dir="{{simulation_wdir}}")
 @task(returns=1, time_out=3600)
-def simulation(name_sim, simulation_wdir, **kwargs):
+def simulation(case_name, simulation_wdir, **kwargs):
     return
 
 
