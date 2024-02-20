e2b1f5ac7201f8bb549b08e853949d998ab49848
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-02-20 10:54:52 +0100

    update

--- PHASES/MESHER/permeability_mesher/RVEgen2Alya.py
+++ PHASES/MESHER/permeability_mesher/RVEgen2Alya.py
@@ -60,7 +60,7 @@ def permeability_mesher(**kwargs):
 
 @task(returns=1)
 def RVEgen2Alya(simulation_wdir, num_cases, density, viscosity, volume_fraction, tipo_fallo, w_tow, h_tow, L_pro, n_elements_gap, n_elements_towsingap,
-                    n_elements_layer, n_layers, angles_tows, n_tows, Lset, ol, ajus_ol, ol_left, ol_right, AlyaSet, debug):
+                    n_elements_layer, n_layers, angles_tows, n_tows, Lset, ol, ajus_ol, ol_left, ol_right, AlyaSet, debug, **kwargs):
     # Get the start time
     st = time.time()
     # --------------------------------------------
