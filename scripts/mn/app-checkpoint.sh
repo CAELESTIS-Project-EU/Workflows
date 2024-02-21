098fa961ad479f33e2aacbf0f25b5f2c6f114c6e
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-02-21 12:25:59 +0100

    update

--- scripts/mn/app-checkpoint.sh
+++ scripts/mn/app-checkpoint.sh
@@ -8,10 +8,37 @@ QOS=$7
 INSTALL_DIR=$8
 BRANCH=$9
 DATA_DIR=${10}
+gOPTION=${11}
+tOPTION=${12}
+dOPTION=${13}
+
 module load compss/3.3
 # shellcheck disable=SC2164
 cd $EXECUTION_FOLDER
-enqueue_compss -d -g -t --worker_working_dir=$PWD --scheduler=es.bsc.compss.scheduler.orderstrict.fifo.FifoTS --job_execution_dir=$EXECUTION_FOLDER --log_dir=$EXECUTION_FOLDER --qos=$QOS --exec_time=$EXEC_TIME --pythonpath=$PYTHONPATH --num_nodes=$NUM_NODES --worker_in_master_cpus=16 --checkpoint_params=instantiated.group:1 --checkpoint=es.bsc.compss.checkpoint.policies.CheckpointPolicyInstantiatedGroup --checkpoint_folder=$EXECUTION_FOLDER/tmp/checkpointing/ $INSTALL_DIR/$BRANCH/WORKFLOWS/api.py $WORKFLOW_FOLDER/$NAMEFILE $EXECUTION_FOLDER $DATA_DIR
+
+# Construct the enqueue_compss command based on user options
+enqueue_compss_cmd="enqueue_compss --worker_working_dir=$PWD --scheduler=es.bsc.compss.scheduler.orderstrict.fifo.FifoTS --job_execution_dir=$EXECUTION_FOLDER --log_dir=$EXECUTION_FOLDER --qos=$QOS --exec_time=$EXEC_TIME --pythonpath=$PYTHONPATH --num_nodes=$NUM_NODES --worker_in_master_cpus=16"
+
+# Add -g option if specified
+if [ "$gOPTION" = "true" ]; then
+  enqueue_compss_cmd="$enqueue_compss_cmd -g"
+fi
+
+# Add -t option if specified
+if [ "$tOPTION" = "true" ]; then
+  enqueue_compss_cmd="$enqueue_compss_cmd -t"
+fi
+
+# Add -d option if specified
+if [ "$dOPTION" = "true" ]; then
+  enqueue_compss_cmd="$enqueue_compss_cmd -d"
+fi
+
+# Add the rest of the command
+enqueue_compss_cmd="$enqueue_compss_cmd $INSTALL_DIR/$BRANCH/WORKFLOWS/api.py $WORKFLOW_FOLDER/$NAMEFILE $EXECUTION_FOLDER $DATA_DIR"
+
+# Execute the command
+$enqueue_compss_cmd
 
 echo DONEEE
 
