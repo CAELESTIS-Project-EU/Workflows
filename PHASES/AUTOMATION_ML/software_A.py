4cbfb60f9131206f35404bbfbf89648c26b3d9e5
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-06 15:39:04 +0100

    update

--- PHASES/AUTOMATION_ML/software_A.py
+++ PHASES/AUTOMATION_ML/software_A.py
@@ -1,3 +1,7 @@
+from pycompss.api.task import task
+from pycompss.api.parameter import *
+
+@task(returns=1)
 def run( **kwargs):
     print("Run Software A with ", kwargs.values())
     return
--- PHASES/AUTOMATION_ML/software_B.py
+++ PHASES/AUTOMATION_ML/software_B.py
@@ -1,3 +1,8 @@
+from pycompss.api.task import task
+from pycompss.api.parameter import *
+
+
+@task(returns=1)
 def run(**kwargs):
     print("Run Software B with ", kwargs.values())
     return
