c57b5f102b97de67580a3bfbf4107413665b8214
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-11 17:45:45 +0100

    ok

--- WORKFLOWS/api.py
+++ WORKFLOWS/api.py
@@ -27,9 +27,6 @@ def get_file_extension(file_path):
     return extension
 
 if __name__ == '__main__':
-    """path = "/home/rcecco/BSCprojects/templates_yaml/PERMEABILITY/test_Permeability.yaml"
-    execution_folder = "/home/rcecco/BSCprojects/testCODE/"
-    data_folder = "/home/rcecco/BSCprojects/testCODE/"""
     path = str(sys.argv[1])
     execution_folder = str(sys.argv[2])
     data_folder = str(sys.argv[3])
