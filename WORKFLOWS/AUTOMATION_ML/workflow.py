a24951264c0bfa5b8757a8790033e08af9e09a59
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-11 17:15:18 +0100

    update

--- WORKFLOWS/AUTOMATION_ML/workflow.py
+++ WORKFLOWS/AUTOMATION_ML/workflow.py
@@ -1,4 +1,4 @@
-from pycompss.api.api import compss_wait_on
+#from pycompss.api.api import compss_wait_on
 from PHASES.utils.utilsAML import args_values_AML
 from PHASES.utils import phase
 import os
@@ -6,10 +6,10 @@ import os
 
 def execution(execution_folder, data_folder, phases, inputs, outputs, parameters):
     out1 = phase.run(args_values_AML.get_values(phases.get("postSIM"), inputs, outputs, parameters, data_folder, locals()))
-    out1=compss_wait_on(out1)
+    #out1=compss_wait_on(out1)
     print(out1)
     out2 = phase.run(args_values_AML.get_values(phases.get("simulation"), inputs, outputs, parameters, data_folder, locals()))
-    out2 = compss_wait_on(out2)
+    #out2 = compss_wait_on(out2)
     print(out2)
     return
 
