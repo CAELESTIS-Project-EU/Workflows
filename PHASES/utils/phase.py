0ad4e715a2fb2d13cea181572417b7df6393ebc6
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-11 17:37:34 +0100

    update

--- PHASES/utils/phase.py
+++ PHASES/utils/phase.py
@@ -6,6 +6,11 @@ def run(phase_info, **kwargs):
     if len(phase_info) != 2:
         raise ValueError("Invalid phase_info tuple. Expected (phase_function, phase_args).")
     if isinstance(phase_info, tuple):
+        phase_function, phase_args = phase_info
+        module_call, function_call = split_string_at_last_dot(phase_function)
+        module = importlib.import_module(module_call)
+        return getattr(module, function_call)(**phase_args, **kwargs)
+    elif isinstance(phase_info, dict):
         phase_function = phase_info.get("type")
         phase_args = phase_info.get("arguments")
         module_call, function_call = split_string_at_last_dot(phase_function)
@@ -20,7 +25,6 @@ def run(phase_info, **kwargs):
             module = importlib.import_module(module_call)
             values.append(getattr(module, function_call)(**phase_args, **kwargs))
         return values
-
 
 def split_string_at_last_dot(input_string):
     last_dot_index = input_string.rfind('.')
