9362fe8ec698e9a4e4e4d6472001db45faa3efc4
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-11 15:14:46 +0100

    update

--- WORKFLOWS/api.py
+++ WORKFLOWS/api.py
@@ -9,30 +9,16 @@ from PHASES.utils.utilsAML import xml_to_yaml
 
 def workflow(path, execution_folder, data_folder):
     extension=get_file_extension(path)
-    if extension==".yaml":
-        with open(path) as f:
-            data = yaml.load(f, Loader=yaml.FullLoader)
-            workflow_type=data.get("workflow_type")
-            parameters=data.get("parameters")
-            inputs=data.get("inputs")
-            outputs = data.get("outputs")
-            phases = data.get("phases")
-            module_call, function_call = phase.split_string_at_last_dot(workflow_type)
-            module = importlib.import_module(module_call)
-            getattr(module, function_call)(execution_folder, data_folder, phases, inputs, outputs, parameters)
-    elif extension==".aml" or extension==".xml":
-        print(path)
-        data = xml_to_yaml.execution(path)
-        print(data)
-        workflow_type = data.get("workflow_type")
-        parameters = data.get("parameters")
-        inputs = data.get("inputs")
+    with open(path) as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        workflow_type=data.get("workflow_type")
+        parameters=data.get("parameters")
+        inputs=data.get("inputs")
         outputs = data.get("outputs")
         phases = data.get("phases")
         module_call, function_call = phase.split_string_at_last_dot(workflow_type)
         module = importlib.import_module(module_call)
         getattr(module, function_call)(execution_folder, data_folder, phases, inputs, outputs, parameters)
-
     return
 
 
