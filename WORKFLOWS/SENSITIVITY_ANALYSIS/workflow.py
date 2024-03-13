60160be6a97cee9c64270c7c1dfa21a51691c9a6
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-13 15:44:10 +0100

    update

--- WORKFLOWS/SENSITIVITY_ANALYSIS/workflow.py
+++ WORKFLOWS/SENSITIVITY_ANALYSIS/workflow.py
@@ -7,10 +7,7 @@ import os
 
 
 def execution(execution_folder, data_folder, phases, inputs, outputs, parameters):
-    param=args_values.get_values(phases.get("sampler"), inputs, outputs, parameters, data_folder, locals())
-    print("PARAM")
-    print(param)
-    sample_set = phase.run(param)
+    sample_set = phase.run(args_values.get_values(phases.get("sampler"), inputs, outputs, parameters, data_folder, locals()))
     sample_set = compss_wait_on(sample_set)
     original_name_sim = parameters.get("original_name_sim")
     results_folder = execution_folder + "/results/"
