d3aec28cc4c07e3181d31a86744038aca8c4658f
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-15 15:57:06 +0100

    Update

--- WORKFLOWS/PERMEABILITY/workflow.py
+++ WORKFLOWS/PERMEABILITY/workflow.py
@@ -7,8 +7,7 @@ import os
 
 
 def execution(execution_folder, data_folder, phases, inputs, outputs, parameters):
-    sample_set = phase.run(
-        args_values.get_values(phases.get("sampler"), inputs, outputs, parameters, data_folder, locals()))
+    sample_set = phase.run(phases.get("sampler"), inputs, outputs, parameters, data_folder, locals())
     sample_set = compss_wait_on(sample_set)
     original_name_sim = parameters.get("original_name_sim")
     results_folder = execution_folder + "/results/"
@@ -20,17 +19,11 @@ def execution(execution_folder, data_folder, phases, inputs, outputs, parameters
         values = sample_set[i, :]
         case_name = "case_" + str(i + 1)
         simulation_wdir = execution_folder + "/SIMULATIONS/" + case_name
-        name_sim = phase.run(
-            args_values.get_values(phases.get("mesher"), inputs, outputs, parameters, data_folder, locals()))
-        sim_out = phase.run(
-            args_values.get_values(phases.get("sim"), inputs, outputs, parameters, data_folder, locals()), out=name_sim)
-        post_p_out = phase.run(
-            args_values.get_values(phases.get("post_process"), inputs, outputs, parameters, data_folder, locals()),
-            out=sim_out)
+        name_sim = phase.run(phases.get("mesher"), inputs, outputs, parameters, data_folder, locals())
+        sim_out = phase.run(phases.get("sim"), inputs, outputs, parameters, data_folder, locals(), out=name_sim)
+        post_p_out = phase.run(phases.get("post_process"), inputs, outputs, parameters, data_folder, locals(),out=sim_out)
     simulation_wdir = execution_folder + "/SIMULATIONS/"
-    sim_out = phase.run(
-        args_values.get_values(phases.get("join_cases"), inputs, outputs, parameters, data_folder, locals()),
-        out=post_p_out)
+    sim_out = phase.run(phases.get("join_cases"), inputs, outputs, parameters, data_folder, locals(),out=post_p_out)
     return
 
 
