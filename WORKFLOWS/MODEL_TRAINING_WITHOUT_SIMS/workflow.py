624ae0394dba227d0de4557c708a928d1d4930dc
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-15 15:55:34 +0100

    Update

--- WORKFLOWS/MODEL_TRAINING_WITHOUT_SIMS/workflow.py
+++ WORKFLOWS/MODEL_TRAINING_WITHOUT_SIMS/workflow.py
@@ -5,6 +5,6 @@ def execution(execution_folder, data_folder, phases, inputs, outputs, parameters
     results_folder = execution_folder + "/results/"
     if not os.path.isdir(results_folder):
         os.makedirs(results_folder)
-    kernel= phase.run(args_values.get_values(phases.get("kernel_generation"), inputs, outputs, parameters, data_folder, locals()))
-    phase.run(args_values.get_values(phases.get("training"), inputs, outputs, parameters, data_folder, locals()), out=kernel)
+    kernel= phase.run(phases.get("kernel_generation"), inputs, outputs, parameters, data_folder, locals())
+    phase.run(phases.get("training"), inputs, outputs, parameters, data_folder, locals(), out=kernel)
     return
