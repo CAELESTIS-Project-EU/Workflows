9436829f441a86041567551974eb5deda80d627d
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-15 15:47:46 +0100

    Update

--- WORKFLOWS/api.py
+++ WORKFLOWS/api.py
@@ -27,7 +27,7 @@ def get_file_extension(file_path):
     return extension
 
 if __name__ == '__main__':
-    path = "/home/rcecco/BSCprojects/templates_yaml/AML/multiple_chain.yaml"
-    execution_folder = "/home/rcecco/BSCprojects/templates_yaml/MONTECARLO_sld/OHT_Validation_D2.yaml"
-    data_folder = "/home/rcecco/BSCprojects/templates_yaml/MONTECARLO_sld/OHT_Validation_D2.yaml"
+    path = str(sys.argv[1])
+    execution_folder = str(sys.argv[2])
+    data_folder = str(sys.argv[3])
     workflow(path, execution_folder, data_folder)
