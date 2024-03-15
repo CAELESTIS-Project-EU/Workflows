36400f152935e760ded74bc8c28b0b9c50b79b55
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-03-15 15:56:15 +0100

    Update

--- WORKFLOWS/SENSITIVITY_ANALYSIS/workflow.py
+++ WORKFLOWS/SENSITIVITY_ANALYSIS/workflow.py
@@ -1,13 +1,12 @@
 import numpy as np
 from pycompss.api.task import task
 from pycompss.api.parameter import *
-from PHASES.utils import args_values, phase
+from PHASES.utils import phase
 from pycompss.api.api import compss_wait_on
 import os
 
 
 def execution(execution_folder, data_folder, phases, inputs, outputs, parameters):
-    #sample_set = phase.run(args_values.get_values(phases.get("sampler"), inputs, outputs, parameters, data_folder, locals()))
     sample_set = phase.run(phases.get("sampler"), inputs, outputs, parameters,  data_folder, locals())
     sample_set = compss_wait_on(sample_set)
     original_name_sim = parameters.get("original_name_sim")
@@ -20,17 +19,12 @@ def execution(execution_folder, data_folder, phases, inputs, outputs, parameters
         values = sample_set[i, :]
         name_sim = original_name_sim + "-s" + str(i)
         simulation_wdir = execution_folder + "/SIMULATIONS/" + name_sim + "/"
-        #prepare_out = phase.run(args_values.get_values(phases.get("prepare_data"), inputs, outputs, parameters, data_folder, locals()))
         prepare_out = phase.run(phases.get("prepare_data"), inputs, outputs, parameters, data_folder, locals())
-        #sim_out = phase.run(args_values.get_values(phases.get("sim"), inputs, outputs, parameters, data_folder, locals()),out=prepare_out)
         sim_out=phase.run(phases.get("sim"), inputs, outputs, parameters, data_folder, locals(), out=prepare_out)
-        #new_y = phase.run(args_values.get_values(phases.get("post_process"), inputs, outputs, parameters, data_folder, locals()),out=sim_out)
         new_y = phase.run(phases.get("post_process"), inputs, outputs, parameters, data_folder, locals(), out=sim_out)
         y.append(new_y)
-    #out_post=phase.run(args_values.get_values(phases.get("post_process_merge"), inputs, outputs, parameters, data_folder, locals()))
     out_post=phase.run(phases.get("post_process_merge"), inputs, outputs, parameters, data_folder, locals())
     write_file(results_folder, y, "yFile.npy")
-    #phase.run(args_values.get_values(phases.get("sensitivity"), inputs, outputs, parameters, data_folder, locals()), out=out_post)
     phase.run(phases.get("sensitivity"), inputs, outputs, parameters, data_folder, locals(),out=out_post)
     return
 
