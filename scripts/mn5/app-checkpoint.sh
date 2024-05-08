725ba0b86bab93d971a6cb55e4322ce957e4c0f5
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-05-08 10:30:07 +0200

    update

---
+++ scripts/mn5/app-checkpoint.sh
@@ -0,0 +1,44 @@
+USER=$1
+NAMEFILE=$2
+WORKFLOW_FOLDER=$3
+EXECUTION_FOLDER=$4
+NUM_NODES=$5
+EXEC_TIME=$6
+QOS=$7
+INSTALL_DIR=$8
+BRANCH=$9
+DATA_DIR=${10}
+gOPTION=${11}
+tOPTION=${12}
+dOPTION=${13}
+
+module load compss/3.3
+# shellcheck disable=SC2164
+cd $EXECUTION_FOLDER
+
+# Construct the enqueue_compss command based on user options
+enqueue_compss_cmd="enqueue_compss --worker_working_dir=$PWD --project_name=cns100 --output_profile=$EXECUTION_FOLDER/results/time --checkpoint_params=instantiated.group:1 --checkpoint=es.bsc.compss.checkpoint.policies.CheckpointPolicyInstantiatedGroup --checkpoint_folder=$EXECUTION_FOLDER/tmp/checkpointing/ --scheduler=es.bsc.compss.scheduler.orderstrict.fifo.FifoTS --job_execution_dir=$EXECUTION_FOLDER --log_dir=$EXECUTION_FOLDER --qos=$QOS --exec_time=$EXEC_TIME --pythonpath=$PYTHONPATH --num_nodes=$NUM_NODES --worker_in_master_cpus=16"
+
+# Add -g option if specified
+if [ "$gOPTION" = "true" ]; then
+  enqueue_compss_cmd="$enqueue_compss_cmd -g"
+fi
+
+# Add -t option if specified
+if [ "$tOPTION" = "true" ]; then
+  enqueue_compss_cmd="$enqueue_compss_cmd -t"
+fi
+
+# Add -d option if specified
+if [ "$dOPTION" = "true" ]; then
+  enqueue_compss_cmd="$enqueue_compss_cmd -d"
+fi
+
+# Add the rest of the command
+enqueue_compss_cmd="$enqueue_compss_cmd $INSTALL_DIR/$BRANCH/WORKFLOWS/api.py $WORKFLOW_FOLDER/$NAMEFILE $EXECUTION_FOLDER $DATA_DIR"
+
+# Execute the command
+$enqueue_compss_cmd
+
+echo DONEEE
+
---
+++ scripts/mn5/loadModule.sh
@@ -0,0 +1,20 @@
+DEST_PATH="$1"
+export ALYA_BIN=/gpfs/projects/cns100/alya/builds/Alya_mn5g.x
+export ALYA_PROCS=112
+export ALYA_PPN=112
+
+module purge
+# python modules
+module load intel impi  mkl hdf5 python/3.12.1
+module unload intel impi
+# alya modules
+module load gcc ucx/1.15.0-gcc openmpi/4.1.5-gcc
+# gmsh modules
+module load intel gmsh/4.12.2
+# COMPSs module
+module load COMPSs/3.3
+
+
+
+
+export PYTHONPATH=$DEST_PATH:/gpfs/projects/cns100/pips/python3.12/site-packages/:$INSTALL_DIR:$PYTHONPATH:/gpfs/projects/cns100/tests/install_workflow_coupontool/
