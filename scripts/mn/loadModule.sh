016e0687843702fa34fd2de68b42451ca5759f5e
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-05-08 14:26:55 +0200

    update

--- scripts/mn/loadModule.sh
+++ scripts/mn/loadModule.sh
@@ -1,9 +1,9 @@
 DEST_PATH="$1"
-export ALYA_BIN=/gpfs/projects/bsce81/alya/builds/Alya_mn4_sp.x
+export ALYA_BIN=/gpfs/projects/bsc19/CAELESTIS/alya/builds/Alya_mn4_sp.x
 export ALYA_PROCS=48
 export ALYA_PPN=48
 export gmshBinFile=/gpfs/projects/bsce81/gmsh/gmsh-4.11.1-Linux64/bin/gmsh
-export gmsh2alya=/gpfs/projects/bsce81/alya/builds/gmsh2alya.pl
+export gmsh2alya=/gpfs/projects/bsc19/CAELESTIS/alya/builds/gmsh2alya.pl
 module purge
 module load intel/2020.1
 module load impi/2018.4
@@ -14,4 +14,3 @@ module load python/3.9.10
 
 
 export PYTHONPATH=$DEST_PATH:$PYTHONPATH
-
