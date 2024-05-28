e841f623eb44602665bb65d3cf3f6c03fb5fbcda
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-05-28 16:34:01 +0200

    Update

--- PHASES/MODEL_TRAINING/twinkle.py
+++ PHASES/MODEL_TRAINING/twinkle.py
@@ -9,12 +9,14 @@ from pycompss.api.binary import binary
 import numpy as np
 
 
-@task()
+@task(X=COLLECTION_IN, Y=COLLECTION_IN)
 def twinkle(X, Y, Kfold_divisions, training_params, kernel, results_folder, **kwargs):
     params = {}
     for key, value in training_params.items():
         params[key] = value
     estimate_Twinkle = kernel
+    print("HERE")
+    print(f"estimate_Twinkle: {estimate_Twinkle}")
     searcher = GridSearchCV(estimate_Twinkle, params, cv=Kfold_divisions)
     searcher.fit(X,Y)
     df = pd.DataFrame(searcher.cv_results_)
