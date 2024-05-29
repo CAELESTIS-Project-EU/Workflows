db950e561d29ac7dd47c3176aed88fcb90137834
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-05-29 10:13:49 +0200

    Update

--- PHASES/MODEL_TRAINING/PARAMETERS/TwinkleEstimator.py
+++ PHASES/MODEL_TRAINING/PARAMETERS/TwinkleEstimator.py
@@ -4,10 +4,8 @@ import numpy as np
 from sklearn.base import BaseEstimator
 from pycompss.api.parameter import *
 from pycompss.api.task import task
-from pycompss.api.container import container
-from pycompss.api.binary import binary
-import pandas as pd
-from sklearn.metrics import r2_score
+from PHASES.MODEL_TRAINING.twinkle import twinkle_train, twinkle_score, twinkle_predict, post_twinkle
+
 
 @task(returns=1)
 def gen_param(execution_folder, template, **kwargs):
@@ -50,7 +48,7 @@ class TwinkleMyEstimator(BaseEstimator):
         save_file(X._blocks, file_temp)
         twinkle_train(file_temp, self.template, self.romFile, self.gtol, self.ttol, self.terms, self.alsiter,
                       self.wflag, working_dir=self.execution_folder)
-
+        return
 
     def set_params(self, **kwargs):
         # Iterate over the keyword arguments and set the attributes if they exist
@@ -83,39 +81,3 @@ class TwinkleMyEstimator(BaseEstimator):
 @task(x=COLLECTION_IN, data_set_file=FILE_OUT)
 def save_file(x, data_set_file):
     np.savetxt(data_set_file, np.block(x), delimiter=";")
-
-
-
-
-
-@container(engine="SINGULARITY", options="-e", image="/home/bsc/bsc019518/MN4/bsc19518/Permeability/testPerm/Twinkle_DisLib/twinkle.sif")
-@binary(binary="/Twinkle/runTwinkle", args="-file {{inputFile}} -out {{template_outFile}} -gtol {{gtol}} -ttol {{ttol}} -terms {{terms}} -alsiter {{alsiter}} -wflag {{wflag}}", working_dir="{{working_dir}}")
-@task(inputFile=FILE_IN, romFile=FILE_OUT)
-def twinkle_train(inputFile, template_outFile, romFile, gtol, ttol, terms, alsiter, wflag, working_dir, **kwargs):
-    pass
-
-
-
-
-@container(engine="SINGULARITY", options="-e", image="/home/bsc/bsc019518/MN4/bsc19518/Permeability/testPerm/Twinkle_DisLib/twinkle.sif")
-@binary(binary="/Twinkle/runTwinkle", args="-rom {{romFile}} -eval {{evalFile}} -out {{template_outFile}}", working_dir="{{working_dir}}")
-@task(romFile=FILE_IN, evalFile=FILE_IN, outFile=FILE_OUT)
-def twinkle_predict(romFile, evalFile, outFile, template_outFile, working_dir, **kwargs):
-    pass
-
-
-@task(result_file=FILE_IN, returns=1)
-def post_twinkle(result_file):
-    try:
-        data = pd.read_csv(result_file, skiprows=1, delim_whitespace=True, names=['data', 'prediction'])
-    except FileNotFoundError:
-        raise Exception(f"The file {result_file} does not exist.")
-    return data['prediction'].to_numpy()
-
-
-@task(y_blocks=COLLECTION_IN, returns=1)
-def twinkle_score(y_blocks, y_pred):
-    y_true=np.block(y_blocks)
-    if len(y_true)!=len(y_pred):
-        return 0
-    return r2_score(y_true, y_pred)
