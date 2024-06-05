9d18d72e53cb14520eb0b11cf2dd8f23499491ab
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-06-05 10:04:50 +0200

    Update

--- WORKFLOWS/MODEL_TRAINING_WITHOUT_SIMS/workflow.py
+++ WORKFLOWS/MODEL_TRAINING_WITHOUT_SIMS/workflow.py
@@ -6,6 +6,10 @@ def execution(execution_folder, data_folder, phases, inputs, outputs, parameters
     if not os.path.isdir(results_folder):
         os.makedirs(results_folder)
     X, Y = phase.run(phases.get("load"), inputs, outputs, parameters, data_folder, locals())
-    kernel= phase.run(phases.get("kernel_generation"), inputs, outputs, parameters, data_folder, locals())
-    phase.run(phases.get("model_creation"), inputs, outputs, parameters, data_folder, locals(), out=kernel)
+    print(f"X shape: {X.shape}")
+    print(f"X _n_blocks: {X._n_blocks}")
+    print(f"X len blocks: {len(X._blocks)}")
+    print(f"X len block 0: {len(X._blocks[0])}")
+    #kernel= phase.run(phases.get("kernel_generation"), inputs, outputs, parameters, data_folder, locals())
+    #phase.run(phases.get("model_creation"), inputs, outputs, parameters, data_folder, locals(), out=kernel)
     return
