1944b07816a123304a6967f06e3e8f6ad5e3a916
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-06-07 11:03:55 +0200

    update twinkle save file fit

--- PHASES/MODEL_TRAINING/PARAMETERS/TwinkleEstimator.py
+++ PHASES/MODEL_TRAINING/PARAMETERS/TwinkleEstimator.py
@@ -117,7 +117,7 @@ def save_file_predict(x, data_set_file):
 
 @task(x=COLLECTION_IN, y=COLLECTION_IN, data_set_file=FILE_OUT)
 def save_file_fit(x, max_min, y, n_inps, i, data_set_file):
-    combined_y= np.concatenate((np.block(y), max_min[:, n_inps+i]), axis=0)
+    combined_y= np.concatenate((np.block(y), max_min[:, [n_inps+i]]), axis=0)
     combined_data = np.concatenate((np.block(x), max_min[:, :n_inps]), axis=0)
     combined= np.append((combined_data,combined_y), axis=1)
     np.savetxt(data_set_file, combined, delimiter=";")
