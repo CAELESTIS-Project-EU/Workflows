910e15760eb35924727f9de3e85abaf164a577a8
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-06-13 11:02:04 +0200

    update

--- PHASES/POSTSIMULATION/permeability_alya.py
+++ PHASES/POSTSIMULATION/permeability_alya.py
@@ -31,6 +31,8 @@ def postProcessPermeability(**kwargs):
     kwargs['density'] = values['Density']
     kwargs['viscosity'] = values['Viscosity']
     kwargs['n_capas'] = len(kwargs['angulos_tows'])
+    kwargs['n_elementos_gap']= values['n_elements_gap']
+    kwargs['n_elementos_towsingap'] = 100
     del kwargs['values']
     return postproCaso(**kwargs)
 
@@ -38,32 +40,35 @@ def postProcessPermeability(**kwargs):
 
 
 @task(out=COLLECTION_IN, returns=1)
-def postproCaso(simulation_wdir, case_name, w_tow, L_pro, angulos_tows, n_tows, n_capas, Lset, gravity, density, viscosity, **kwargs):
+def postproCaso(simulation_wdir, case_name, w_tow, L_pro, angulos_tows, n_tows, n_capas, Lset, gravity, density, viscosity, n_elementos_gap, n_elementos_towsingap, **kwargs):
     num_caso=extract_number(case_name)
     archivo_x = 'x-flow/'+ case_name+ '-element.nsi.set'
     archivo_y = 'y-flow/'+ case_name+ '-element.nsi.set'
     archivo_z = 'z-flow/'+ case_name+ '-element.nsi.set'
+
     archivos = [archivo_x, archivo_y, archivo_z]
     for angulo in angulos_tows:
         if angulo == 0 or angulo == 90:
             angulo_dist_0_90 = 90.0
         else:
             angulo_dist_0_90 = angulo
+            break
     Ldom = n_tows * (w_tow + L_pro) / math.sin(math.radians(angulo_dist_0_90))
-    dimYc = int(Ldom / Lset)
-    dimXc = int(Ldom / Lset)
-    nsetscapa = dimXc * dimYc
+    seed_XY = L_pro / n_elementos_gap
+    if L_pro == 0:
+        seed_XY = w_tow / n_elementos_towsingap
+    n_nodos = round(Ldom / seed_XY + 1)
+    nodos = n_nodos - 1
+    EpS = int(np.ceil(Lset / (L_pro / n_elementos_gap)))
+    LsetReal = EpS * (Ldom / nodos)
     dimZc = n_capas
     nOut = 6
-    # joint_sets = np.zeros([len(archivos), dimXc-2, dimYc-2, dimZc, nOut])
 
-    sets = np.zeros([dimXc * dimYc * dimZc, 1 + nOut * 3])
     for n, direccion in enumerate(archivos):
-        # direccion = 'x-element.nsi.set'
         header = []
         last_iter_R = []
         last_iter = []
-        with open(simulation_wdir + "/"+direccion, 'r') as f:
+        with open(simulation_wdir + direccion, 'r') as f:
             for row in f:
                 if row != '# START\n':
                     header.append(row)
@@ -77,24 +82,28 @@ def postproCaso(simulation_wdir, case_name, w_tow, L_pro, angulos_tows, n_tows, 
 
         for row in reversed(last_iter_R):
             last_iter.append(row)
+        if n == 0:
+            sets = np.zeros([len(last_iter), 1 + nOut * 3])
+            sets[:, 0] = np.asarray(last_iter, dtype=float)[:, 0]
         sets[:, 1 + n * nOut:1 + (n + 1) * nOut] = np.asarray(last_iter, dtype=float)[:, 2:]
-        sets[:, 0] = np.asarray(last_iter, dtype=float)[:, 0]
     extrasets = []
     iset = 1
-    for order in range(3):
+    dimXc = math.sqrt(len(last_iter) / dimZc)
+    dimYc = dimXc
+    nsetscapa = dimXc * dimYc
+    for order in range(5):
         for set1 in sets:
-            if (order <= (set1[0] % nsetscapa) % dimXc < (dimXc - order)) and (
-                    order <= int((set1[0] % nsetscapa) / dimYc) < (dimXc - order)):
+            if (order <= ((set1[0] - 1) % nsetscapa) % dimXc < (dimXc - order)) and (
+                    order <= int(((set1[0] - 1) % nsetscapa) / dimYc) < (dimXc - order)):
                 neighbours = AdjSets(set1[0] - 1, dimXc, order)
                 setstomerge = sets[neighbours.astype(int)]
                 setstomerge[:, 0] = iset
-                extrasets.append(np.r_[num_caso, iset, Lset * (order * 2 + 1), np.mean(setstomerge, axis=0)[1:]])
+                extrasets.append(np.r_[num_caso, iset, LsetReal * (order * 2 + 1), np.mean(setstomerge, axis=0)[1:]])
                 iset += 1
     extrasets = np.asarray(extrasets)
 
-    np.savetxt(simulation_wdir + '/set_results.csv', extrasets, delimiter=',')
-    # setfiles = os.listdir('output/Caso_'+str(num_caso)+'/msh')
-    ruta_caso = simulation_wdir
+    np.savetxt(simulation_wdir + 'set_results.csv', extrasets, delimiter=',')
+    ruta_caso = 'output/Caso_' + str(num_caso)
     ruta_mesh = os.path.join(ruta_caso, 'msh')
     set_outputs = np.loadtxt(os.path.join(ruta_caso, 'set_results.csv'), delimiter=',')
     for file in os.listdir(ruta_mesh):
@@ -108,14 +117,17 @@ def postproCaso(simulation_wdir, case_name, w_tow, L_pro, angulos_tows, n_tows, 
                     splitline = re.split('\s+', new_line)[:-1]
                     set_inputs.append(splitline)
             set_inputs = np.asarray(set_inputs, dtype=float)
+            set_output_dict = {tuple(row[:2]): row[3:] for row in set_outputs}
             for line_in in set_inputs:
-                line_out = set_outputs[np.all(set_outputs[:, :3] == line_in[:3], axis=1), 3:]
-                if len(line_out) != 0:
-                    set_results.append(np.r_[line_in, line_out[0]])
+                key = tuple(line_in[:2])
+                if key in set_output_dict:
+                    line_out = set_output_dict[key]
+                    set_results.append(np.r_[line_in, line_out])
             set_results = np.asarray(set_results)
             for line in set_results:
-                evl, evt0, evt1 = Permeability_Calculation_sim(gravity, density, viscosity, line[-18:])
-                toRom.append(np.r_[line[:-18], evl, evt0, evt1])
+                evl, evt0, evt1, Permeability_tensor = Permeability_Calculation_sim(gravity, density, viscosity,
+                                                                                    line[-18:])
+                toRom.append(np.r_[line[:-18], evl, evt0, evt1, Permeability_tensor])
             np.savetxt(os.path.join(ruta_caso, 'toRom' + file), np.asarray(toRom))
     return np.asarray(toRom)
 
@@ -139,24 +151,30 @@ import os
 # import pandas as pd
 
 @task(out=COLLECTION_IN, returns=1)
-def JoinCases(simulation_wdir, outputName, results_folder,  out, erase_previous = True, **kwargs):
-    outputFile= os.path.join(results_folder, outputName)
-    if erase_previous:
-        open(outputFile, 'w').close()
-    # simulation_wdir = 'output/'
-    lista_casos = os.listdir(simulation_wdir)
-    s1 = 'variable1s1;variable2s1;variable3s1;variable4s1;variable5s1;'
-    s2 = 'variable1s2;variable2s2;variable3s2;variable4s2;variable5s2;'
-    s3 = 'variable1s3;variable2s3;variable3s3;variable4s3;variable5s3'
-    with open(outputFile, 'a') as f:
-        f.write('simulacion;set;' + s1 + s2 + s3 + '\n')
-    """for caso in lista_casos:
-        with open(simulation_wdir + caso + '/set_results.csv', 'r') as f:
-            new_case = f.readlines()
-            # Reemplazar comas por punto y coma (;) en cada línea
-            modified_lines = [line.replace(',', ';') for line in new_case]"""
-    for l in out:
-        l=l.tolist()
-        with open(outputFile, 'a') as f:
-            f.writelines(';'.join(map(str,l)))
+def JoinCases(simulation_wdir, results_folder, erase_previous = True, **kwargs):
+    name_files = ['toRomallsets.csv']
+    debug_headers = 'case;set;Lset;x;y;z;layer;h_tow;FVF_tow;FVF_set;llori;ilori;ulori;'
+    gap_headers = 'gap;dgap;sgap;'
+    ol_headers1 = 'ol;'
+    ol_headers2 = 'dol;sol;adjgap;dadjgap;sadjgap;'
+    out_headers = 'k1;k2;k3;pv1x;pv1y;pv1z;pv2x;pv2y;pv2z;Perm_xx;Perm_xy;Perm_xz;Perm_yy;Perm_yz;Perm_zz'
+    for geom in name_files:
+        content = []
+        for caso in os.listdir(simulation_wdir):
+            if geom.replace('csv', 'txt') in os.listdir(os.path.join(simulation_wdir, str(caso))):
+                file = os.path.join(simulation_wdir, str(caso), geom.replace('csv', 'txt'))
+                with open(file, 'r') as f:
+                    for line in f:
+                        content.append(line.split(' '))
+        all_cases = np.asarray(content,dtype = float)
+        n_capas = 6
+        oris = np.zeros([len(all_cases),3])
+        for i, sets in enumerate(all_cases):
+            oris[i,1] = sets[10+int((sets[6])%n_capas)]
+            oris[i,0] = sets[10+int((sets[6]-1)%n_capas)]
+            oris[i,2] = sets[10+int((sets[6]+1)%n_capas)]
+
+        todebug = np.c_[all_cases[:,:10],oris,all_cases[:,10+n_capas+7:]]
+        file_out2 = geom.replace('toRom', 'RawResults')
+        np.savetxt(os.path.join(results_folder,file_out2),todebug, delimiter = ';', header=debug_headers+gap_headers+ol_headers1+ol_headers2+out_headers, comments='')
     return
