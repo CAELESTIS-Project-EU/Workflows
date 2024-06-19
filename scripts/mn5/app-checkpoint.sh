4009c1647cf71eaff7625e2fa9743d6c2fee0fee
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-06-19 11:34:33 +0200

    Update

--- scripts/mn5/app-checkpoint.sh
+++ scripts/mn5/app-checkpoint.sh
@@ -16,7 +16,7 @@ dOPTION=${13}
 cd $EXECUTION_FOLDER
 
 # Construct the enqueue_compss command based on user options
-enqueue_compss_cmd="enqueue_compss --worker_working_dir=$PWD --project_name=cns100 --output_profile=$EXECUTION_FOLDER/results/time --checkpoint_params=instantiated.group:1 --checkpoint=es.bsc.compss.checkpoint.policies.CheckpointPolicyInstantiatedGroup --checkpoint_folder=$EXECUTION_FOLDER/tmp/checkpointing/ --scheduler=es.bsc.compss.scheduler.orderstrict.fifo.FifoTS --job_execution_dir=$EXECUTION_FOLDER --log_dir=$EXECUTION_FOLDER --qos=$QOS --exec_time=$EXEC_TIME --pythonpath=$PYTHONPATH --num_nodes=$NUM_NODES --worker_in_master_cpus=16"
+enqueue_compss_cmd="enqueue_compss --worker_working_dir=$PWD --project_name=cns100 --output_profile=$EXECUTION_FOLDER/results/time --checkpoint_params=instantiated.group:1 --checkpoint=es.bsc.compss.checkpoint.policies.CheckpointPolicyInstantiatedGroup --checkpoint_folder=$EXECUTION_FOLDER/tmp/checkpointing/ --scheduler=es.bsc.compss.scheduler.orderstrict.fifo.FifoTS --job_execution_dir=$EXECUTION_FOLDER --log_dir=$EXECUTION_FOLDER --qos=$QOS --exec_time=$EXEC_TIME --pythonpath=$PYTHONPATH --num_nodes=$NUM_NODES --worker_in_master_cpus=112"
 
 # Add -g option if specified
 if [ "$gOPTION" = "true" ]; then
