aa9a53a53a54c8102cd01b631e212890232b1463
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-06-21 16:38:32 +0200

    Update

--- PHASES/MODEL_TRAINING/PARAMETERS/TwinkleEstimator.py
+++ PHASES/MODEL_TRAINING/PARAMETERS/TwinkleEstimator.py
@@ -83,7 +83,7 @@ class TwinkleMyEstimator(BaseEstimator):
     def set_params(self, **kwargs):
         name_folder=""
         if 'i' in kwargs:
-            name_folder = "variable_"+str(kwargs['i'])+"__"
+            name_folder = "variable_"+str(self.variable)+"__"
 
         for key, value in kwargs.items():
             if hasattr(self, key):
