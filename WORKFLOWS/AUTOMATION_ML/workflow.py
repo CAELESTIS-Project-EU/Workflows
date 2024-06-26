cd78506b11279e8d42cd3330b4874365a7d8a4e3
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-06-26 14:38:06 +0200

    update

--- WORKFLOWS/AUTOMATION_ML/workflow.py
+++ WORKFLOWS/AUTOMATION_ML/workflow.py
@@ -3,7 +3,8 @@ from PHASES.utils import phase
 from PHASES.AUTOMATION_ML import check_license
 
 def execution(execution_folder, data_folder, phases, inputs, outputs, parameters):
-    if check_license:
+    check_license_run=check_license()
+    if check_license_run:
         out1 = phase.run(phases.get("Preprocess"), inputs, outputs, parameters, data_folder, locals())
         out1=compss_wait_on(out1)
         out2 = phase.run(phases.get("Simulation"), inputs, outputs, parameters, data_folder, locals())
