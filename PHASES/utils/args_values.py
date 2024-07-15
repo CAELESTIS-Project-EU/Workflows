1372a523eb0828c296b85fe935d9310e3b12d803
RiccardoCecco <ceccoriccardo1997@gmail.com>
2024-07-15 11:28:33 +0200

    update error XML

--- PHASES/utils/args_values.py
+++ PHASES/utils/args_values.py
@@ -45,22 +45,17 @@ def get_arguments_XML(phase_list, inputs, outputs, params, data_folder, symbol_t
             else:
                 pattern = r'\{(.*?)\}'
                 matches = re.findall(pattern, str(value))
-                print(f"matches: {matches}")
                 if matches:
                     for matchA in matches:
-                        print(f"matchA: {matchA}")
                         first_part, second_part = extract_parts(matchA)
                         switch_result = switch_values(first_part, second_part, inputs, outputs, params, data_folder, symbol_table, key)
                         output = switch_result.get(key)  # Extract the value associated with the key
-                        print(f"output: {output}")
-                        print(f"value 1: {value}")
                         value = value.replace("{" + matchA + "}", output)  # Replace directly
-                        print(f"value 2: {value}")
                     args.update({key: value})
                 else:
                     args.update({key: value})
 
-        return {"type": typePhase, "parameters": args}
+        return {"type": typePhase, "arguments": args}
 
 
 
--- PHASES/utils/phase.py
+++ PHASES/utils/phase.py
@@ -9,30 +9,33 @@ def run(phase, inputs, outputs, parameters, data_folder, local_vars, **kwargs):
     print(f"data_folder: {data_folder}")
     print(f"local_vars: {local_vars}")
     # Assuming phase_info is a tuple (phase_function, phase_args)
-    if isinstance(phase, dict):
-        phase_info=args_values.get_values(phase, inputs, outputs, parameters, data_folder, local_vars)
-        phase_function, phase_args = phase_info
-        module_call, function_call = split_string_at_last_dot(phase_function)
-        module = importlib.import_module(module_call)
-        return getattr(module, function_call)(**phase_args, **kwargs)
-    elif isinstance(phase, list) and len(phase)==1:
-        phase_info = args_values.get_values(phase, inputs, outputs, parameters, data_folder, local_vars)
-        phase_function = phase_info.get("type")
-        phase_args = phase_info.get("arguments")
-        module_call, function_call = split_string_at_last_dot(phase_function)
-        module = importlib.import_module(module_call)
-        return getattr(module, function_call)(**phase_args, **kwargs)
-    else:
-        values = []
-        for p in phase:
-            phase_info = args_values.get_values([p], inputs, outputs, parameters, data_folder, local_vars)
-            phase_function = phase_info.get("type")
-            phase_args = phase_info.get("arguments")
-            #phase_args =args_values.get_values(phase_info, inputs, outputs, parameters, data_folder, local_vars)
+    try:
+        if isinstance(phase, dict):
+            phase_info=args_values.get_values(phase, inputs, outputs, parameters, data_folder, local_vars)
+            phase_function, phase_args = phase_info
             module_call, function_call = split_string_at_last_dot(phase_function)
             module = importlib.import_module(module_call)
-            values.append(getattr(module, function_call)(**phase_args, **kwargs))
-        return values
+            return getattr(module, function_call)(**phase_args, **kwargs)
+        elif isinstance(phase, list) and len(phase)==1:
+            phase_info = args_values.get_values(phase, inputs, outputs, parameters, data_folder, local_vars)
+            phase_function = phase_info.get("type")
+            phase_args = phase_info.get("arguments")
+            module_call, function_call = split_string_at_last_dot(phase_function)
+            module = importlib.import_module(module_call)
+            return getattr(module, function_call)(**phase_args, **kwargs)
+        else:
+            values = []
+            for p in phase:
+                phase_info = args_values.get_values([p], inputs, outputs, parameters, data_folder, local_vars)
+                phase_function = phase_info.get("type")
+                phase_args = phase_info.get("arguments")
+                #phase_args =args_values.get_values(phase_info, inputs, outputs, parameters, data_folder, local_vars)
+                module_call, function_call = split_string_at_last_dot(phase_function)
+                module = importlib.import_module(module_call)
+                values.append(getattr(module, function_call)(**phase_args, **kwargs))
+            return values
+    except Exception as e:
+        print(f"[ ERROR ]: An exception occurred: {e}")
 
 def split_string_at_last_dot(input_string):
     last_dot_index = input_string.rfind('.')
