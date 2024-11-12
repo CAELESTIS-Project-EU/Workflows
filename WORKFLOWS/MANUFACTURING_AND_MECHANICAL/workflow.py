4920e34264aa7b7d7a6fad84a4abef9ab8ab18ec
mgarci5 <mgarci5@bsc.es>
2024-11-12 17:56:30 +0100

    Add post process for alya simulation

--- WORKFLOWS/MANUFACTURING_AND_MECHANICAL/workflow.py
+++ WORKFLOWS/MANUFACTURING_AND_MECHANICAL/workflow.py
@@ -1,3 +1,8 @@
+import numpy as np
+from PHASES.utils import phase
+from pycompss.api.api import compss_wait_on
+from pycompss.api.task import task
+from pycompss.api.parameter import *
 import os
 
 from pycompss.api.api import compss_wait_on
@@ -23,6 +28,7 @@ def execution(execution_folder, data_folder, phases, inputs, outputs, parameters
     if check_license_run:
         print("CHECK LICENSE DONE")
         a = 0
+        y = []
         for index, row in df.iterrows():
             a += 1
             line_number = 'line' + str(a)
@@ -30,18 +36,37 @@ def execution(execution_folder, data_folder, phases, inputs, outputs, parameters
             if not os.path.isdir(simulation_wdir):
                 os.makedirs(simulation_wdir)
             DoE_line = dict(zip(DoE_names, row))
-            #sim_out = phase.run(phases.get("Simulation"), inputs, outputs, parameters, data_folder, locals())
-            t1 = time.time()
-            prepare_out = phase.run(phases.get("Prepare Data"), inputs, outputs, parameters,
-                      data_folder, locals(), index=index)
-            print ("TIMEEEEEEEEEE:", time.time() - t1)
-            phase.run(phases.get("Simulation2"), inputs, outputs, parameters,
-                      data_folder, locals(), out=prepare_out)
-            if "PostProcess" in phases:
-                phase.run(phases.get("PostProcess"), inputs, outputs, parameters, data_folder, locals())
 
+            """
+            sim_out = phase.run(phases.get("Simulation"), inputs, outputs, parameters, data_folder, locals())
+            
+            if "PostProcess" in phases:
+                phase.run(phases.get("PostProcess"), inputs, outputs, parameters, data_folder, locals(), out=sim_out)
+            """
+
+            prepare_out = phase.run(phases.get("Prepare Data"), inputs, outputs, parameters,
+                      data_folder, locals(), index=index) #out=sim_out
+            alya_out = phase.run(phases.get("Simulation2"), inputs, outputs, parameters,
+                      data_folder, locals(), out=prepare_out)
+
+            if "PostProcess2" in phases:
+                new_y = phase.run(phases.get("PostProcess2"), inputs, outputs, parameters, data_folder, locals(), out=alya_out)
+                y.append(new_y)
+
+        write_file(results_folder, y, "yFile.npy")
     else:
         print("LICENSE IS NOT RUNNING!")
     return
 
 
+@task(elements=COLLECTION_IN)
+def write_file(output_folder, elements, nameFile, **kwargs):
+    model_file= os.path.join(output_folder, nameFile)
+    write(model_file, elements)
+
+
+def write(file, element):
+    with open(file, 'wb') as f3:
+        np.save(f3, element)
+        f3.close()
+    return
