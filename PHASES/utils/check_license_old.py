024db5e3c4b8a541a900ad7541900ad9ebbc65cc
Gerard <gerard.guillamet@bsc.es>
2025-07-16 06:19:58 +0200

    Check license file is moved to utils #16

---
+++ PHASES/utils/check_license_old.py
@@ -0,0 +1,26 @@
+from pycompss.api.task import task
+from pycompss.api.parameter import *
+from pycompss.api.container import container
+from pycompss.api.binary import binary
+import subprocess
+
+
+def check_license():
+    script = "/gpfs/projects/bsce81/check_license_server_new.sh"
+
+    try:
+        # Execute the shell script
+        result = subprocess.run([script], capture_output=True, text=True)
+
+        # Check the exit code of the script
+        if result.returncode == 0:
+            print("License Server is already running or started successfully.")
+            return True
+        else:
+            print("License Server is not running and could not be started. Check the log file for details.")
+            print(result.stdout)
+            print(result.stderr)
+            return False
+    except subprocess.CalledProcessError as e:
+        print(f"Script execution failed: {e}")
+        return False
